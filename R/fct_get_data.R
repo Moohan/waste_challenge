0cbce5a9b744450e84f65ca0fbd11029c4bc820a
James McMahon <james.mcmahon@phs.scot>
2022-02-01 12:45:49 +0000

    Cache the data so we only have to fetch it once

--- R/fct_get_data.R
+++ R/fct_get_data.R
@@ -7,6 +7,13 @@
 #' @noRd
 get_data <- function() {
   googlesheets4::gs4_deauth()
-  googlesheets4::read_sheet("1oG6DS4NSYvsSYxYFUxFtGD_u1hY2LdEXWk_E7Zu-NX8", .name_repair = janitor::make_clean_names) %>%
-    dplyr::mutate(dplyr::across(c(household, tidyselect::contains("bin_emptied")), forcats::as_factor))
+
+  sheet_id <- "1oG6DS4NSYvsSYxYFUxFtGD_u1hY2LdEXWk_E7Zu-NX8"
+
+  xfun::cache_rds(
+    googlesheets4::read_sheet(sheet_id, .name_repair = janitor::make_clean_names) %>%
+      dplyr::mutate(dplyr::across(
+        c(household, tidyselect::contains("bin_emptied")), forcats::as_factor
+      ))
+  )
 }

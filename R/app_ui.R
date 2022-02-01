04c3a16ed0a0113e46f4cfb7fdd02f5c5d52b398
James McMahon <james.mcmahon@phs.scot>
2022-02-01 12:23:13 +0000

    Add waiter for spinner whilst loading

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,6 +10,7 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
+    waiter::autoWaiter(),
     dashboardPage(
       dashboardHeader(title = "Waste Challenge - February 2022"),
       dashboardSidebar(sidebarMenu(

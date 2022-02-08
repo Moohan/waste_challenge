8fb0ac2800620198c72f00ff83558256a73377a7
James McMahon <jamesmcmahon0@gmail.com>
2022-02-08 22:15:09 +0000

    Recalibrate landfill bin weight

--- R/app_server.R
+++ R/app_server.R
@@ -29,7 +29,7 @@ app_server <- function(input, output, session) {
   bin_weights <-
     data.frame(
       household = c("James & Camille", "Amy", "Amy & Jonny"),
-      weight_of_landfill_waste_kg = c(1.3, NA, NA),
+      weight_of_landfill_waste_kg = c(1.293, NA, NA),
       weight_of_plastic_recycling_kg = c(0.7, NA, NA),
       weight_of_food_waste_kg = c(0.272, NA, NA),
       weight_of_other_recycling_kg = c(0.544, NA, NA), 

4b7f1c928080bd1d2c491ce005f392cbf1f28ba8
James McMahon <jamesmcmahon0@gmail.com>
2022-02-11 17:22:48 +0000

    Do cumsum by household

--- R/fct_sum_waste.R
+++ R/fct_sum_waste.R
@@ -28,7 +28,9 @@ sum_waste <- function(data, type, bin, bin_weights) {
     dplyr::left_join(bin_weights) %>%
     tidyr::replace_na(list(bin_weight = 0)) %>%
     dplyr::mutate(weight = pmax(.data$weight - .data$bin_weight, 0)) %>%
+    group_by(household) %>%
     dplyr::mutate(cummulative_weight = cumsum_bin(weight = .data$weight, bin = {{ bin }})) %>%
+    ungroup() %>%
     dplyr::mutate(type = stringr::str_replace(.data$type, "weight_of_(.+?)_kg", "\\1") %>%
                     stringr::str_replace_all("_", " ") %>%
                     stringr::str_to_title())

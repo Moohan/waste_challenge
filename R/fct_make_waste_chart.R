0358a4b6d3a1e99ddc109fa6ffad6291ef809488
James McMahon <jamesmcmahon0@gmail.com>
2022-02-14 12:43:58 +0000

    Make function only return a plot for easier testing

--- R/fct_make_waste_chart.R
+++ R/fct_make_waste_chart.R
@@ -8,50 +8,46 @@
 #' @import scales
 #' @noRd
 make_waste_chart <- function(data, input) {
-  renderPlot({
+  plot <- data %>%
+    ggplot(aes(x = timestamp,
+               y = cummulative_weight))
 
-    plot <- data %>%
-      ggplot(aes(x = timestamp,
-                 y = cummulative_weight))
+  days <-
+    as.integer(ceiling(max(data$timestamp) - min(data$timestamp)))
 
-    days <- as.integer(ceiling(max(data$timestamp) - min(data$timestamp)))
+  if (input$plotType == "One plot") {
+    # Draw the main waste plot
+    plot <- plot +
+      geom_area(aes(
+        colour = household,
+        fill = household,
+        alpha = type
+      )) +
+      scale_alpha_discrete(name = "Waste type", range = c(0.5, 0.8)) +
+      scale_color_brewer(name = "Household",
+                         type = "qual",
+                         palette = "Set2") +
+      scale_fill_brewer(name = "Household",
+                        type = "qual",
+                        palette = "Set2") +
+      scale_x_datetime("Date",
+                       date_breaks = paste(ceiling(days / 5), "day"),
+                       date_labels = "%d-%b")
 
-    if (input$plotType == "One plot") {
-      # Draw the main waste plot
-      plot <- plot +
-        geom_area(aes(
-          colour = household,
-          fill = household,
-          alpha = type
-        )) +
-        scale_alpha_discrete(name = "Waste type", range = c(0.5, 0.8)) +
-        scale_color_brewer(name = "Household",
-                           type = "qual",
-                           palette = "Set2") +
-        scale_fill_brewer(name = "Household",
-                          type = "qual",
-                          palette = "Set2") +
-        scale_x_datetime("Date",
-                         date_breaks = paste(ceiling(days / 5), "day"),
-                         date_labels = "%d-%b")
+  } else {
+    plot <- plot +
+      geom_area(aes(fill = type,)) +
+      scale_fill_brewer(name = "Waste type",
+                        type = "qual",
+                        palette = "Paired") +
+      scale_x_datetime("Date",
+                       date_breaks = paste(ceiling(days / 3), "day"),
+                       date_labels = "%d-%b") +
+      facet_wrap("household")
+  }
 
-    } else {
-      plot <- plot +
-        geom_area(aes(
-          fill = type,
-        )) +
-        scale_fill_brewer(name = "Waste type",
-                           type = "qual",
-                          palette = "Paired") +
-        scale_x_datetime("Date",
-                         date_breaks = paste(ceiling(days / 3), "day"),
-                         date_labels = "%d-%b") +
-        facet_wrap("household")
-    }
-
-    plot +
-      scale_y_continuous("Weight (Kg)", ) +
-      ggtitle(paste(unique(data$type), collapse = " and ")) +
-      theme_minimal()
-  })
+  plot +
+    scale_y_continuous("Weight (Kg)",) +
+    ggtitle(paste(unique(data$type), collapse = " and ")) +
+    theme_minimal()
 }

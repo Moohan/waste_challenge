2274221d13e52584674cc336ae45efd0ea8076c0
James McMahon <james.mcmahon@phs.scot>
2022-02-15 08:48:06 +0000

    Extrapolate unfilled landfill/plastic

--- R/fct_get_data.R
+++ R/fct_get_data.R
@@ -15,5 +15,37 @@ get_data <- function() {
       household, tidyselect::contains("bin_emptied")
     ), forcats::as_factor)) %>%
     arrange(household, timestamp) %>%
-    fix_data()
+    fix_data() %>%
+    dplyr::group_by(household) %>%
+    dplyr::mutate(single_bin = xor(
+      is.na(weight_of_landfill_waste_kg),
+      is.na(weight_of_plastic_recycling_kg)
+    )) %>%
+    dplyr::mutate(last_has_all_data = max(dplyr::if_else(!single_bin, timestamp, as.POSIXct(NA)), na.rm = TRUE)) %>%
+    tidyr::fill(weight_of_landfill_waste_kg,
+                weight_of_plastic_recycling_kg) %>%
+    dplyr::mutate(across(
+      c(bin_emptied, bin_emptied_2),
+      ~ dplyr::if_else(
+        single_bin &
+          is.na(.x) &
+          timestamp >= last_has_all_data,
+        "No",
+        as.character(.x)
+      )
+    )) %>%
+    dplyr::mutate(
+      weight_of_landfill_waste_kg = dplyr::if_else(
+        !single_bin &
+          is.na(bin_emptied),
+        NA_real_,
+        weight_of_landfill_waste_kg
+      ),
+      weight_of_plastic_recycling_kg = dplyr::if_else(
+        !single_bin &
+          is.na(bin_emptied_2),
+        NA_real_,
+        weight_of_plastic_recycling_kg
+      )
+    )
 }
